932b4bbfe17a0993d5a393007a830a24317ffe0e
madison sorah <madisonsorah@gmail.com>
2021-11-01 17:42:07 -0400

    commit tree 7656d8f8db997613f8ced2bbdddac8c7e583eccd

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 8;
+let num4 = 2;
+const mod = num3 / num4;
+
+const max = Math.max(16, 17, 18, 19, 20);
